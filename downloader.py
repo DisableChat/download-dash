95659b12354d4f6b1b75ee1efaf8ec249a77d295
wes <wryder@pretalen.com>
2018-05-21 12:20:10 -0400

    horse now races

--- downloader.py
+++ downloader.py
@@ -1,35 +1,41 @@
 import requests
 from urllib import request
-
+import sys
+import urllib
 ##
 # Determining file size before download and also downloading file from url
-# *note* this basic implementation to pull from url and also determine file
-# size pre download
+# While also displaying the horse running(downloading animation)
 ##
 
-url = 'http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv' # hard coding url from now
+url = 'https://www.sample-videos.com/img/Sample-jpg-image-5mb.jpg' # hard coding url from now
+url2 = 'https://www.sample-videos.com/img/Sample-jpg-image-1mb.jpg'
+file_name = "download.data" # declaring name of file in directory
 
+#printing the size of the file from url
 def size_info(url):
-    resp = requests.get(url)
-    print('Size Pre Download (recorded) :  ', resp.headers['content-length']) # Size of file pulled from url
-    print('Actual size of the file bytes:   123,637') # Size of the file from inspecting from local directory
+    print ("opening url:", url)
+    url_size = urllib.request.urlopen(url)
+    print('File Size: ', url_size.headers['content-length'], 'bytes') #printing file size
 
+
+# downloading file and displaying progress
 def download_data(url):
-    response = request.urlopen(url)
-    file = response.read()
-    file = str(file)
-    lines = file.split("\\n")
-    destination_url = r'links.csv' # raw format of file
-    fx = open(destination_url, 'w')
-    for line in lines:
-        fx.write(line + "\n")
-        print('Downloaded and Created a file in directory...')
-    fx.close()
+    with open(file_name, "wb") as f: #opening file and writing as binary
+        print ("Downloading File My Guy and saving as %s" % file_name)
+        response = requests.get(url, stream=True) #setting up the requests.get from download url
+        total_length = response.headers.get('content-length')
+        data_length = 0
+        total_length = int(total_length)
+        for data in response.iter_content(chunk_size = 1024):
+            data_length += len(data) #  updating data length
+            f.write(data) # file write data
+            done = int(50 * data_length / total_length) # done is 50 * data_length divided by total length
+            #printing the horse running animation
+            sys.stdout.write("\rstart |%s|:]%s| finish" % ('-' * done, ' ' * (50-done)))
+            sys.stdout.flush() #makes the terminal output faster by flushing buffer
 
-
-def main():
+def main(url):
     size_info(url)
     download_data(url)
-
-
-main()
+    print('\n')
+main(url)
