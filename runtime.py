04477689444d1634737bfecad4932212b68d0cce
wes <wryder@pretalen.com>
2018-05-23 09:22:15 -0400

    bug

--- runtime.py
+++ runtime.py
@@ -18,18 +18,25 @@ url3= 'https://www.sample-videos.com/img/Sample-jpg-image-30mb.jpg'
 
 #attempting to create thread so we can print the player1.data_length()
 def main():
+    bool = True
     player1 = dc.Downloader()
     player1.set_total_length(url)
     thread1 = Thread(target=player1.set_data_length, args=(url,))
     thread1.start()
     start_time = time.time()
+    #player1.set_data_length(url)
+    #print(player1.get_data_length())
 
-    while(player1.get_data_length() != player1.get_total_length()):
+    while(bool == True):
         #print(player1.get_data_length())
         done = int(50 * player1.data_length / player1.total_length) # done is 50 * data_length divided by total length
         sys.stdout.write("\rRate:   %.3f mbs Percent Downloaded: %s%%  start |%s|:]%s| finish!" %
             (round((player1.data_length/(time.time()- start_time)) /1024/1024, 3), 2*done, '-' * done, ' ' * (50-done)))
         sys.stdout.flush() #makes the terminal output faster by flushing buffer
         #time.sleep(.2)
+        if(player1.get_data_length() == player1.get_total_length()):
+            break
+
     #dc.download_data(url, player1.data_length)
+
 main()
