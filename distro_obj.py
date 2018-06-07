15ddb909a178b71c4c9424f55d13ffc68047b3d5
wes <wryder@pretalen.com>
2018-06-07 16:55:32 -0400

    added new func

--- distro_obj.py
+++ distro_obj.py
@@ -1,23 +1,24 @@
-
 # Directory info
 distro_list_dir     = '/home/wes/Source/download-dash/distros.txt'
-file_directory      = '/home/wes/Source/download-dash/mirrorlists/'
+file_directory      = '/home/wes/Source/download-dash/'
 
 # Text file names
-arch            = 'arch.txt'
-centos          = 'centos.txt'
-debian          = 'debian.txt'
-dragonflybsd    = 'dragonflybsd.txt'
-fedora          = 'fedora.txt'
-gentoo          = 'gentoo.txt'
-mageia          = 'mageia.txt'
-mint            = 'mint.txt'
-nethbsd         = 'netbsd.txt'
-openbsd         = 'openbsd.txt'
-sabayon         = 'sabayon.txt'
-slackware       = 'slackware.txt'
-ubuntu          = 'ubuntu.txt'
+arch            = 'mirrorlists/arch.txt'
+centos          = 'mirrorlists/centos.txt'
+debian          = 'mirrorlists/debian.txt'
+dragonflybsd    = 'mirrorlists/dragonflybsd.txt'
+fedora          = 'mirrorlists/fedora.txt'
+gentoo          = 'mirrorlists/gentoo.txt'
+mageia          = 'mirrorlists/mageia.txt'
+mint            = 'mirrorlists/mint.txt'
+nethbsd         = 'mirrorlists/netbsd.txt'
+openbsd         = 'mirrorlists/openbsd.txt'
+sabayon         = 'mirrorlists/sabayon.txt'
+slackware       = 'mirrorlists/slackware.txt'
+ubuntu          = 'mirrorlists/ubuntu.txt'
 
+# Array to hold distro obj's
+distro_lib_array = []
 
 ##
 # Distro takes the txt files and creates an array of the object Distro
@@ -25,23 +26,43 @@ ubuntu          = 'ubuntu.txt'
 ##
 class Distro:
 
-    # Array to hold distro obj's
-    distro_lib_array = ''
-
     # Distro object vars
-    distro           = ''
-    path             = ''
-    address          = ''
-    filenames        = ''
+    distro          = ''
+    path            = ''
+    address         = []
+    filenames       = ''
 
-    def open_file(self, file_directory, file):
+    distro_loc      = []
+
+    # Opens the txt file containing the distro addresses and then
+    def create_address_array(self, file_directory, file):
 
         with open(file_directory + file, 'r') as fp:
+            self.address = fp.readlines()
+            for lines in range(len(self.address)):
+                self.address[lines] = self.address[lines].strip('\n')
+
+
+    def get_distro_spacing(self, distro_list_dir):
+
+        counter = 0
+
+        with open(distro_list_dir, 'r') as fp:
             array = fp.readlines()
-            for lines in range(len(array)):
-                print(array[lines].strip('\n'))
+            for line in range(len(array)):
+                if(array[line].find(':') != -1):
+                    self.distro_loc.append(line)
+                    counter += 1
+
+            for line in range(len(self.distro_loc)):
+                print(self.distro_loc[line])
+
+            print ("Number of Distros: ", counter)
+
 
 
 
 test = Distro()
-test.open_file(file_directory, fedora)
+test.create_address_array(file_directory, fedora)
+print(test.address[1] + 'hi')
+test.get_distro_spacing(distro_list_dir)
